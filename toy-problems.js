a098a790431e564cb44245ddb2e9ebe2d41cfbce
Dean Asadi <eng.de86@gmail.com>
2018-12-16 07:18:12 -0600

    findUniq

--- toy-problems.js
+++ toy-problems.js
@@ -556,3 +556,23 @@ function findNextSquare(n) {
 }
 
 console.log(findNextSquare(625));
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//   There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains more than 3 numbers.
+
+var newArr = []
+
+function findUniq(arr) {
+  for( let i = 0; i < arr.length; i++ ) {
+    if(arr[0] != arr[i]){
+      newArr.push(arr[i])
+      
+    }
+  }
+}
+findUniq([ 1, 1, 1, 2, 1, 1 ])
