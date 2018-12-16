9bc2ec2e4eeb020b035cd09c7a0f7159cb35bc8e
Dean Asadi <eng.de86@gmail.com>
2018-12-15 20:27:35 -0600

    Par

--- toy-problems.js
+++ toy-problems.js
@@ -503,3 +503,21 @@ function findUserPosts( arr, user ) {
 findUserPosts(posts, 'slogesdale9')
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
+// Write a function called validParentheses that takes a string of parentheses, and determines if the order of the parentheses is valid. validParentheses should return true if the string is valid, and false if it's invalid.
+
+// Examples: 
+// validParentheses( "()" ) => returns true 
+// validParentheses( ")(()))" ) => returns false 
+// validParentheses( "(" ) => returns false 
+// validParentheses( "(())((()())())" ) => returns true 
+
+// All input strings will be nonempty, and will only consist of open parentheses '(' and/or closed parentheses ')'
+
+
+//sol: #1
+
+function validParentheses(str){
+  return str.split(")").length  === str.split("(").length  
+ }
+ validParentheses("))(())((")
+ 
