260c2939db6ae8326aacbbf14818f5f17472bc51
Dean Asadi <eng.de86@gmail.com>
2019-01-05 16:06:07 -0600

    format a string

--- toy-problems.js
+++ toy-problems.js
@@ -814,3 +814,14 @@ function bubble(arr) {
 
   return arr;
 }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+//format a string
+// Given: an array containing hashes of names
+
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+// Example:
+
+list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+ returns 'Bart, Lisa & Maggie'
