77eeca20ce0cb20490877090caaed1d5d9e7b370
Dean Asadi <eng.de86@gmail.com>
2019-01-09 17:48:14 -0600

    firstNonRepeatingLetter†

--- toy-problems.js
+++ toy-problems.js
@@ -901,3 +901,7 @@ function findMissing(arr) {
     num += diff;
   }
 }
+
+// Write a function named firstNonRepeatingLetter† that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
